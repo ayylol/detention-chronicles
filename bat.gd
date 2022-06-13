1b01ef0c1412932bc0b6f781a4db4288d080839c
ApogeeS <abdullahsoydan@hotmail.com>
2022-06-12 21:52:41 -0600

    added SFX for bat swing

--- bat.gd
+++ bat.gd
@@ -7,6 +7,18 @@ extends Spatial
 onready var batanimation = $AnimationPlayer
 var swingonestate = true  
 
+
+# audio-related stuff - begin
+
+onready var bat_swing_player = $BatSwingPlayer
+
+const BAT_SWING = {
+	0: preload("res://assets/audio/SFX_bat_swing_R-L.wav"),
+	1: preload("res://assets/audio/SFX_bat_swing_L-R.wav")
+}
+
+# audio-related stuff - end
+
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 
@@ -23,3 +35,15 @@ func _input(event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+# plays left to right bat swing with a random playback rate
+func play_bat_swing_LtoR():
+	bat_swing_player.set_stream(BAT_SWING[0])
+	bat_swing_player.set_pitch_scale(rand_range(0.9, 1.1))
+	bat_swing_player.play()
+	
+func play_bat_swing_RtoL():
+	bat_swing_player.set_stream(BAT_SWING[1])
+	bat_swing_player.set_pitch_scale(rand_range(0.9, 1.1))
+	bat_swing_player.play()
--- principle.gd
+++ principle.gd
@@ -19,12 +19,13 @@ func _ready():
 	#for anim in anim_player.get_animation_list():
 	#	anim_player.get_animation(anim).set_loop(true)
 	anim_player.play("walk")
-	
 
 func _physics_process(delta):
 	pass
 	#move_and_slide(Vector3(0,0,-5))
 
+
+# plays a random footstep with a random playback rate
 func play_footstep():
 	var footstep_index = randi() % FOOTSTEP.size()
 	if footstep_index == last_footstep_index:
