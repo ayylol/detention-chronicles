0f1dbec9ec03ffdb33844f9d3ac5934d7f42366c
David Garcia Orozco <David.GarOro@protonmail.com>
2022-07-03 19:08:03 -0600

    Implemented ai pathfinding modes

--- principle.gd
+++ principle.gd
@@ -1,8 +1,10 @@
 extends KinematicBody
 enum State {
-	SEARCHING,
-	CHASING,
+	SEARCH,
+	CHASE,
+	PATROL,
 }
+
 export var speed = 10.0
 export var ground_acceleration = 10.0
 export var air_acceleration = 6.0
@@ -18,10 +20,13 @@ const FOOTSTEP = {
 
 var last_footstep_index = 0
 
+var behaviour = State.PATROL
 var path = []
 var path_node = 0
 var max_speed = 8.0
+var gravity = Vector3.DOWN * 9.81
 var points_of_interest = []
+var _target_index
 var _target
 
 onready var nav = get_parent()
@@ -32,21 +37,24 @@ onready var current_speed = max_speed
 func _ready():	
 	for point in $"../NavMesh/PointsOfInterest".get_children():
 		points_of_interest.push_back(point)
-	
-	_target = Player # start moving towards player
 	anim_player.play("walk")
 
 func _physics_process(delta):
+	#print(global_transform.origin)
 	if path_node < path.size():
 		var direction = (path[path_node] - global_transform.origin)
+		direction.y = 0
 		if direction.length() < 1:
 			path_node += 1
 		else:
-			move_and_slide(direction.normalized() * current_speed, Vector3.UP)
+			direction = direction.normalized()
+			var look_dir = lerp(-global_transform.basis.z, direction, delta*10)
+			look_dir.y = global_transform.origin.y
+			look_at(global_transform.origin+look_dir, Vector3.UP)
+			move_and_slide(direction * current_speed + gravity, Vector3.UP)
 
 func move_to(target_pos):
 	path = nav.get_simple_path(global_transform.origin, target_pos)
-	look_at_from_position(global_transform.origin, Vector3(target_pos.x, global_transform.origin.y, target_pos.z), Vector3.UP)
 	path_node = 0
 
 # plays a random footstep with a random playback rate
@@ -61,5 +69,38 @@ func play_footstep():
 	
 	last_footstep_index = footstep_index
 
+func get_target():
+	match behaviour:
+		State.CHASE:
+			_target = Player
+		State.PATROL:
+			if(not _target in points_of_interest):
+				var min_dist = 9223372036854775807
+				var i = 0
+				for point in points_of_interest:
+					var dist = global_transform.origin.distance_squared_to(point.global_transform.origin)
+					if dist < min_dist:
+						_target_index = i
+						_target = point
+						min_dist = dist
+					i+=1
+			else:
+				if global_transform.origin.distance_squared_to(
+					Vector3(_target.global_transform.origin.x, 
+							global_transform.origin.y, 
+							_target.global_transform.origin.z)) < 2:
+					_target_index = (_target_index+1) % points_of_interest.size()
+					_target = points_of_interest[_target_index]
+		State.SEARCH:
+			var min_dist = 9223372036854775807
+			for point in points_of_interest:
+				var dist = Player.global_transform.origin.distance_squared_to(point.global_transform.origin)
+				if dist < min_dist:
+					_target = point
+					min_dist = dist
+		_:
+			_target = Player
+
 func _on_MoveTimer_timeout():
+	get_target()
 	move_to(_target.global_transform.origin)
